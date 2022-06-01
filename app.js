2979d50a210f68cec9344bbbd71418097eb5ef89
Russell Taylor <rctaylorllc@gmail.com>
2022-06-01 16:46:55 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,32 @@ Object.values(dataRow).forEach((val) => {
   );
 });
 
+
 //With this function, we have done the following:
 //Looped through each object in the array
 //Appended a row to the HTML table
 //Added each value from the object into a cell
+
+//add filters 11.5.3
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+let filteredData = tableData;
+d3.selectAll("#filter-btn").on("click", handleClick); //Listen for the Event
+//11.5.4 Use the “If” Statement
+// Check to see if a date was entered and filter the
+  // data using that date.
+if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+};
+// Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+
+// Build the table when the page loads
+buildTable(tableData);
+
+
+};
